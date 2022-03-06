f5331ffa9492f3f1f10609282ff4cb4881c6869a
saenai <paul.cosma97@gmail.com>
2022-03-06 12:44:25 +0200

    fix(privacy): contact email is now a link and headings are not justified

--- client/src/pages/PrivacyPolicy.tsx
+++ client/src/pages/PrivacyPolicy.tsx
@@ -1,22 +1,27 @@
-import { Container, Heading, HStack, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { Container, Heading, HStack, Link, ListItem, Stack, StackProps, Text, UnorderedList, useBreakpointValue } from "@chakra-ui/react";
 import { FC } from "react";
 import Navigation from "../components/Navigation";
+
+const ResponsiveStack: FC<StackProps> = (props) => {
+    const UsedStack = useBreakpointValue({ base: Stack, md: HStack })
+    return <UsedStack {...props} />
+}
 
 const PrivacyPolicy: FC = () => (
     <>
         <Navigation />
-        <Container mt='10' maxW='900px' textAlign='justify'>
-            <HStack mb='5' justifyContent='space-between'>
-                <Heading>Privacy Policy</Heading>
+        <Container my='10' maxW='900px' textAlign='justify'>
+            <ResponsiveStack mb='5' justifyContent='space-between'>
+                <Heading textAlign='left'>Privacy Policy</Heading>
                 <Text>Last updated: March 04, 2022</Text>
-            </HStack>
+            </ResponsiveStack>
 
             <Text>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</Text>
             <Text>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.privacypolicies.com/blog/privacy-policy-template/" target="_blank">Privacy Policy Template</a>.</Text>
-            <Heading mb='3' mt='7'>Interpretation and Definitions</Heading>
-            <Heading size='md' mt='3' mb='2'>Interpretation</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Interpretation and Definitions</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Interpretation</Heading>
             <Text>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</Text>
-            <Heading size='md' mt='3' mb='2'>Definitions</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Definitions</Heading>
             <Text>For the purposes of this Privacy Policy:</Text>
             <UnorderedList>
                 <ListItem>
@@ -53,19 +58,19 @@ const PrivacyPolicy: FC = () => (
                     <Text><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</Text>
                 </ListItem>
             </UnorderedList>
-            <Heading mb='3' mt='7'>Collecting and Using Your Personal Data</Heading>
-            <Heading size='md' mt='3' mb='2'>Types of Data Collected</Heading>
-            <Heading size='sm' mt='2' mb='1'>Personal Data</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Collecting and Using Your Personal Data</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Types of Data Collected</Heading>
+            <Heading textAlign='left' size='sm' mt='2' mb='1'>Personal Data</Heading>
             <Text>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</Text>
             <UnorderedList>
                 <ListItem>Usage Data</ListItem>
             </UnorderedList>
-            <Heading size='sm' mt='2' mb='1'>Usage Data</Heading>
+            <Heading textAlign='left' size='sm' mt='2' mb='1'>Usage Data</Heading>
             <Text>Usage Data is collected automatically when using the Service.</Text>
             <Text>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</Text>
             <Text>When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.</Text>
             <Text>We may also collect information that Your browser sends whenever You visit our Service or when You access the Service by or through a mobile device.</Text>
-            <Heading size='md' mt='3' mb='2'>Use of Your Personal Data</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Use of Your Personal Data</Heading>
             <Text>The Company may use Personal Data for the following purposes:</Text>
             <UnorderedList>
                 <ListItem>
@@ -102,19 +107,19 @@ const PrivacyPolicy: FC = () => (
                 <ListItem><strong>With other users:</strong> when You share personal information or otherwise interact in the public areas with other users, such information may be viewed by all users and may be publicly distributed outside.</ListItem>
                 <ListItem><strong>With Your consent</strong>: We may disclose Your personal information for any other purpose with Your consent.</ListItem>
             </UnorderedList>
-            <Heading size='md' mt='3' mb='2'>Retention of Your Personal Data</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Retention of Your Personal Data</Heading>
             <Text>The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.</Text>
             <Text>The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.</Text>
-            <Heading size='md' mt='3' mb='2'>Transfer of Your Personal Data</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Transfer of Your Personal Data</Heading>
             <Text>Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.</Text>
             <Text>Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.</Text>
             <Text>The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.</Text>
-            <Heading size='md' mt='3' mb='2'>Disclosure of Your Personal Data</Heading>
-            <Heading size='sm' mt='2' mb='1'>Business Transactions</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Disclosure of Your Personal Data</Heading>
+            <Heading textAlign='left' size='sm' mt='2' mb='1'>Business Transactions</Heading>
             <Text>If the Company is involved in a merger, acquisition or asset sale, Your Personal Data may be transferred. We will provide notice before Your Personal Data is transferred and becomes subject to a different Privacy Policy.</Text>
-            <Heading size='sm' mt='2' mb='1'>Law enforcement</Heading>
+            <Heading textAlign='left' size='sm' mt='2' mb='1'>Law enforcement</Heading>
             <Text>Under certain circumstances, the Company may be required to disclose Your Personal Data if required to do so by law or in response to valid requests by public authorities (e.g. a court or a government agency).</Text>
-            <Heading size='sm' mt='2' mb='1'>Other legal requirements</Heading>
+            <Heading textAlign='left' size='sm' mt='2' mb='1'>Other legal requirements</Heading>
             <Text>The Company may disclose Your Personal Data in the good faith belief that such action is necessary to:</Text>
             <UnorderedList>
                 <ListItem>Comply with a legal obligation</ListItem>
@@ -123,22 +128,22 @@ const PrivacyPolicy: FC = () => (
                 <ListItem>Protect the personal safety of Users of the Service or the public</ListItem>
                 <ListItem>Protect against legal liability</ListItem>
             </UnorderedList>
-            <Heading size='md' mt='3' mb='2'>Security of Your Personal Data</Heading>
+            <Heading textAlign='left' size='md' mt='3' mb='2'>Security of Your Personal Data</Heading>
             <Text>The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</Text>
-            <Heading mb='3' mt='7'>Children's Privacy</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Children's Privacy</Heading>
             <Text>Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.</Text>
             <Text>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</Text>
-            <Heading mb='3' mt='7'>Links to Other Websites</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Links to Other Websites</Heading>
             <Text>Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</Text>
             <Text>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</Text>
-            <Heading mb='3' mt='7'>Changes to this Privacy Policy</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Changes to this Privacy Policy</Heading>
             <Text>We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.</Text>
             <Text>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the &quot;Last updated&quot; date at the top of this Privacy Policy.</Text>
             <Text>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</Text>
-            <Heading mb='3' mt='7'>Contact Us</Heading>
+            <Heading textAlign='left' mb='3' mt='7'>Contact Us</Heading>
             <Text>If you have any questions about this Privacy Policy, You can contact us:</Text>
             <UnorderedList>
-                <ListItem>By email: pacstall@protonmail.com</ListItem>
+                <ListItem>By email: <Link href='mailto:pacstall@protonmail.com' color='pink.400'>pacstall@protonmail.com</Link></ListItem>
             </UnorderedList>
         </Container>
     </>
