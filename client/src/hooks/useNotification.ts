b4f2d06135c5a5a07adf0e7ed1dee5e6a8e69013
Paul Cosma <paul.cosma97@gmail.com>
2022-03-11 20:21:21 +0200

    fix(notification): set max limit to 5

--- client/src/hooks/useNotification.ts
+++ client/src/hooks/useNotification.ts
@@ -1,18 +1,32 @@
-import { useToast } from "@chakra-ui/react"
+import { ToastId, useToast } from "@chakra-ui/react"
 import Notification from "../types/notifications"
 
+const duration = 5000;
+let toasts: ToastId[] = []
+const toastLimit = 5;
 
 const useNotification = () => {
     const toast = useToast({
         position: 'top'
     })
     return (notification: Notification) => {
-        toast({
-            status: notification.type,
-            title: notification.title,
-            description: notification.text,
-            variant: 'solid'
-        })
+        (async () => {
+            while (toasts.filter(id => toast.isActive(id)).length >= toastLimit) {
+                await new Promise(resolve => setTimeout(resolve, 100))
+            }
+
+            toasts = toasts.filter(id => toast.isActive(id))
+
+            const id = toast({
+                status: notification.type,
+                title: notification.title,
+                description: notification.text,
+                variant: 'solid',
+                duration
+            })
+
+            toasts.push(id)
+        })()
     }
 }
 
