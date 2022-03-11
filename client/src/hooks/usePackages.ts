7086e5fd299e2b53d3b4d596954f13c232e25a59
Paul Cosma <paul.cosma97@gmail.com>
2022-03-11 21:04:13 +0200

    fix(navigation): browser back no longer adds empty history states

--- client/src/hooks/usePackages.ts
+++ client/src/hooks/usePackages.ts
@@ -1,40 +1,48 @@
 import axios from "axios"
+import React from "react"
 import { useState, useEffect } from "react"
-import { useQueryParams, NumberParam, StringParam } from "use-query-params"
+import { useLocation, useNavigate } from "react-router-dom"
 import serverConfig from "../config/server"
 import { PackageInfoPage } from "../types/package-info"
+
+const useQuery = (): [URLSearchParams, (query: URLSearchParams, replace?: boolean) => void] => {
+    const { search, pathname } = useLocation();
+    const navigate = useNavigate()
+
+    const data = React.useMemo(() => new URLSearchParams(search), [search]);
+    const setData = (query: URLSearchParams, replace = false) => navigate(`${pathname}?${query.toString()}`, { replace })
+
+    return [data, setData]
+}
 
 const usePackages = () => {
     const [data, setData] = useState<PackageInfoPage>(null as any)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
-    const [queryParams, setQueryParams] = useQueryParams({
-        page: {
-            decode: val => +val,
-            encode: val => val,
-            equals: (v1, v2) => v1 == v2
-        },
-        size: NumberParam,
-        sort: StringParam,
-        sortBy: StringParam,
-        filter: StringParam,
-        filterBy: StringParam
-    })
+    const [queryParams, setQueryParams] = useQuery()
 
     useEffect(() => {
-        const url = `/api/packages?page=${queryParams.page || 0}&size=${queryParams.size || 25}&sort=${queryParams.sort || ''}&sortBy=${queryParams.sortBy || 'default'}&filter=${queryParams.filter || ''}&filterBy=${queryParams.filterBy || 'name'}`
+        const url = `/api/packages?page=${queryParams.get('page') || 0}&size=${queryParams.get('size') || 25}&sort=${queryParams.get('sort') || ''}&sortBy=${queryParams.get('sortBy') || 'default'}&filter=${queryParams.get('filter') || ''}&filterBy=${queryParams.get('filterBy') || 'name'}`
         setLoading(true)
         setError(false)
         axios.get<PackageInfoPage>(`${serverConfig.host}${url}`).then(res => {
             setData(res.data)
-            setQueryParams({ page: queryParams.page || 0 })
+
+            if (!queryParams.get('page')) {
+                queryParams.set('page', '0')
+                setQueryParams(queryParams, true)
+            }
             setLoading(false)
         }).catch(() => setError(true))
     }, [queryParams])
 
     const onSearch = (filter: string, filterBy: string) => {
         setLoading(true)
-        setQueryParams({ filter, filterBy, page: 0 })
+
+        queryParams.set('page', '0')
+        queryParams.set('filter', filter)
+        queryParams.set('filterBy', filterBy)
+        setQueryParams(queryParams)
     }
 
     return {
