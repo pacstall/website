64f95f8603c0723468271dce1d86f305faa5b8ba
Paul Cosma <paul.cosma97@gmail.com>
2022-09-22 00:07:38 +0300

    fix: client build & add link to package req

--- client/src/components/packages/PackageList.tsx
+++ client/src/components/packages/PackageList.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Text } from '@chakra-ui/react'
+import { Box, Center, Text, chakra } from '@chakra-ui/react'
 import { FC } from 'react'
 import PackageInfo from '../../types/package-info'
 import PackageTable from './PackageTable'
@@ -7,9 +7,20 @@ const PackageList: FC<{ data: PackageInfo[] }> = ({ data }) => (
     <Box mt='10'>
         <PackageTable packages={data} />
         {data.length === 0 && (
-            <Box mt='5'>
+            <Box my='5'>
                 <Center>
-                    <Text>No packages found</Text>
+                    <Text>
+                        Not finding the App you wanted?{' '}
+                        <chakra.a
+                            href='https://github.com/pacstall/pacstall-programs/issues/new?assignees=&labels=package+request&template=PACKAGE-REQUEST.yml&title=PacReq%3A+%3Cname_of_the_package%3E'
+                            target='_blank'
+                            color='pink.400'
+                            textDecoration='underline'
+                            fontWeight='800'
+                        >
+                            Create a request!
+                        </chakra.a>
+                    </Text>
                 </Center>
             </Box>
         )}
