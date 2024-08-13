63c4f19b4aebabc861c75dabb2b40936d3fbab88
Elsie <hwengerstickel@protonmail.com>
2024-08-13 10:32:37 -0400

    Add ſ to middle english

--- client/src/locale/en-ENM.locale.ts
+++ client/src/locale/en-ENM.locale.ts
@@ -8,80 +8,80 @@ export default <Locale>{
             whyDifferent: {
                 title: 'Why is þis ani different þan any oþer pakagge manager?',
                 description:
-                    'Pacstall noteþ þe stable base of Ubuntu but alloweþ þe to notest ' +
-                    'bledynge egge softeware wiþ litel to no compromises, so þou ne dost ' +
-                    'have to troubleþ þeself wiþ securite pacches or newe fetures.',
+                    'Pacſtall noteþ þe ſtable baſe of Ubuntu but alloweþ þe to noteſt ' +
+                    'bledynge egge ſofteware wiþ litel to no compromiſes, ſo þou ne doſt ' +
+                    'have to troubleþ þeſelf wiþ ſecurite pacches or newe fetures.',
             },
             howItWorks: {
                 title: 'How þenne doþ hit werk?',
                 description:
-                    ' Pacstall takeþ in fyles witen as <0>pacscripts</0> (like' +
-                    ' PKGBUILDs) þat holden þe necessarye contents to bylden pakagges,' +
-                    ' and byldeþ þem in-to executables on þi systeme.',
+                    ' Pacſtall takeþ in fyles witen as <0>pacſcripts</0> (like' +
+                    ' PKGBUILDs) þat holden þe neceſsarye contents to bylden pakagges,' +
+                    ' and byldeþ þem in-to executables on þi ſyſteme.',
             },
         },
-        installationInstructions: 'Installacioun Instrucciouns',
+        installationInstructions: 'Inſtallacioun Inſtrucciouns',
         showcase: {
             title: 'Schewecas',
             packageSearch: 'Pakagge Serchen',
         },
     },
     navbar: {
-        title: 'Pacstall',
+        title: 'Pacſtall',
         contribute: {
             title: 'Contribute',
             workOnFeatures: 'Werken on newe fetures',
-            helpTranslate: 'Helpen wiþ translaciouns',
+            helpTranslate: 'Helpen wiþ tranſlaciouns',
             becomeAMaintainer: 'Becomen a pakagge mayntener',
         },
         social: {
             title: 'Social Nettwerks',
-            discord: 'Discord',
+            discord: 'Diſcord',
             matrix: 'Matrix',
             reddit: 'Reddit',
             lemmy: 'Lemmy',
-            mastodon: 'Mastodon',
+            mastodon: 'Maſtodon',
         },
         browse: {
-            title: 'Browsen Pakagges',
+            title: 'Browſen Pakagges',
         },
         privacy: {
             title: 'Privacie Policie',
         },
-        install: 'Installen',
+        install: 'Inſtallen',
     },
     cookieConsent: {
         title: 'Cookie notice TL;DR',
         paragraphs: [
-            'Hail, we are notynge cookies on ure website.',
-            'We ne doþ wisshe to ȝiven ȝe misledynge ne mistakynge informacion. We oonly noten cookies for essential fetures swich as teme setynges, localisacioun and auþentikacioun.',
+            'Hail, we are notynge cookies on ure webſite.',
+            'We ne doþ wiſshe to ȝiven ȝe miſledynge ne miſtakynge informacion. We oonly noten cookies for eſsential fetures ſwich as teme ſetynges, localiſacioun and auþentikacioun.',
             'Ȝe can rede þe ful privacie policie <0>her <1/></0>.',
-            'By continuenge to noten þis site, ȝe <strong>ȝivest eower agrement to þe privacie policie</strong>.',
+            'By continuenge to noten þis ſite, ȝe <strong>ȝivest eower agrement to þe privacie policie</strong>.',
         ],
         accept: "God be wi' ye",
     },
     packageSearch: {
         dropdown: {
             package: 'Pakagge',
-            packageTooltip: 'Serchen in pakagge names and descripciouns',
+            packageTooltip: 'Serchen in pakagge names and deſcripciouns',
             maintainer: 'Mayntener',
             maintainerTooltip: 'Serchen by mayntener names and emailes',
         },
         table: {
             name: 'Name',
             maintainer: 'Mayntener',
-            version: 'Version',
-            install: 'Install',
+            version: 'Verſion',
+            install: 'Inſtall',
         },
         versionTooltip: {
-            notInRegistry: 'Þis pakagge ne is in þe Repologgye reggistrye',
-            latest: 'Þis pakagge is þe latest version',
+            notInRegistry: 'Þis pakagge ne is in þe Repologgye reggiſtrye',
+            latest: 'Þis pakagge is þe lateſt version',
             patch: 'Þis pakagge haveþ a patche update available',
             minor: 'Þis pakagge haveþ a minor update available',
             major: 'Þis pakagge haveþ a major update available',
             isGit: 'Þis pakagge is a Git pakagge',
         },
-        noResults: 'Ne findynge what ȝe wisshest? <0>Create a request!</0>',
+        noResults: 'Ne findynge what ȝe wiſsheſt? <0>Create a request!</0>',
         search: 'Serchen',
         orphaned: 'Orphaned',
         maintainerTooltip: {
@@ -96,39 +96,39 @@ export default <Locale>{
     packageDetails: {
         table: {
             name: 'Name',
-            version: 'Version',
+            version: 'Verſion',
             maintainer: 'Mayntener',
             dependencies: 'Dependencies',
             requiredBy: 'Required By',
-            lastUpdatedAt: 'Laste Updated At',
+            lastUpdatedAt: 'Laſte Updated At',
         },
         orphaned: 'Orphaned',
         noResults: 'None',
         openInGithub: 'Open In Github',
         view: 'Vewen',
         howToInstall: {
-            title: 'How to Install',
-            step1: 'Steppe 1: Setup Pacstall',
-            step2: 'Steppe 2: Install {{ name }}',
+            title: 'How to Inſtall',
+            step1: 'Steppe 1: Setup Pacſtall',
+            step2: 'Steppe 2: Inſtall {{ name }}',
         },
         dependenciesModal: {
             title: 'Dependencies',
             buildDependencies: 'Bylde Dependencies',
             optionalDependencies: 'Optional Dependencies',
             runtimeDependencies: 'Runtyme Dependencies',
-            pacstallDependencies: 'Pacstall Dependencies',
+            pacstallDependencies: 'Pacſtall Dependencies',
             name: 'Name',
             close: 'Closen',
             provider: 'Provider',
-            noDescription: 'Description ne is available',
-            version: 'Version',
+            noDescription: 'Deſcription ne is available',
+            version: 'Verſion',
         },
         requiredByModal: {
             title: 'Required By',
             name: 'Name',
             provider: 'Provider',
-            close: 'Closen',
-            noDescription: 'Description ne is available',
+            close: 'Cloſen',
+            noDescription: 'Deſcription ne is available',
         },
     },
 }
